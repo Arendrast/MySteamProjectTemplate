23b169941de120f7fff3e01d6dbf6466a3e1c288
Arendrast <120911943+Arendrast@users.noreply.github.com>
2026-05-18 18:56:28 +0500

    Fixes some bugs with new overlap observers

--- Assets/Modules/SharedModule/Runtime/Shared/Scripts/Observers/Overlap/OverlapObserver.cs
+++ Assets/Modules/SharedModule/Runtime/Shared/Scripts/Observers/Overlap/OverlapObserver.cs
@@ -1,34 +1,49 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Modules.SharedModule.Runtime.Shared.Scripts.Observers.Overlap
 {
+    [ExecuteInEditMode]
     public abstract class OverlapObserver : MonoBehaviour, IOverlapObserver
     {
         public IReadOnlyCollection<Collider> CurrentOverlaps => ObserverHelper.CurrentOverlaps;
         public IOverlapEventsProvider EventsProvider => ObserverHelper;
         protected OverlapObserverHelper ObserverHelper { get; private set; }
         protected OverlapGizmosDrawer OverlapGizmosDrawer { get; private set; }
-        
+
         [field: SerializeField] protected OverlapObserverLogicConfig OverlapConfig { get; private set; }
         [SerializeField] private OverlapObserverDebugConfig _debugConfig;
 
         private void Awake()
         {
+            OverlapGizmosDrawer = new OverlapGizmosDrawer(_debugConfig, DrawGizmos);
             ObserverHelper = new OverlapObserverHelper(OverlapConfig, GetAddedOverlapResultsNumber,
                 TryMakeNewOverlapAdded);
-
-            OverlapGizmosDrawer = new OverlapGizmosDrawer(_debugConfig, DrawGizmos);
+        }
+        
+        private void OnDrawGizmos()
+        {
+            if (enabled)
+            {
+                OverlapGizmosDrawer.DrawGizmos(false);
+            }
         }
 
+        private void OnDrawGizmosSelected()
+        {
+            if (enabled)
+            {
+                OverlapGizmosDrawer.DrawGizmos(true);
+            }
+        }
+        
         private void Update() => ObserverHelper.OnUpdate();
         private void OnEnable() => ObserverHelper.OnEnable();
         private void OnDisable() => ObserverHelper.OnDisable();
-        private void OnDrawGizmos() => OverlapGizmosDrawer.DrawGizmos(false);
-        private void OnDrawGizmosSelected() => OverlapGizmosDrawer.DrawGizmos(true);
-        
+
         public void PerformOverlapCheck() => ObserverHelper.PerformOverlapCheck();
-        
+
         protected abstract void DrawGizmos(bool obj);
         protected abstract int GetAddedOverlapResultsNumber(Collider[] overlapResultsBuffer);
 
